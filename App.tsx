810e5ac16431f5780c6c0aa751838fc2212d2591
mrodriguezto <mirdor.dev@gmail.com>
2022-11-11 14:29:53 -0500

    setup: prettier config and theme

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { NativeBaseProvider } from "native-base";
 import StackNavigator from "@navigation/StackNavigator";
+import theme from "@src/theme/index";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <NativeBaseProvider>
+      <NativeBaseProvider theme={theme}>
         <StackNavigator />
       </NativeBaseProvider>
     </NavigationContainer>
